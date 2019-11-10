73ad842019c005ea04bd66ec21a965a0ad5a6bd5
SergiusMagnus <52329866+SergiusMagnus@users.noreply.github.com>
2019-11-10 17:22:54 +0300

    Update Shoot-the-bullet.ino

--- Shoot-the-bullet.ino
+++ Shoot-the-bullet.ino
@@ -1,26 +1,35 @@
+#include <LCD5110_Graph.h>
+
 #include "Field.h"
 #include "playerShip.h"
 
-const int UP = 2;
-const int RIGHT = 3;
-const int DOWN = 4;
-const int LEFT = 5;
+#define UP_PIN 2
+#define RIGHT_PIN 3
+#define DOWN_PIN 4
+#define LEFT_PIN 5
+
+LCD5110 LCD(9, 10, 11, 12, 13);
 
 playerShip Player;
 Field Field;
 
+void input();
+void output();
+
+const int width = 13;
+const int height = 20;
+
 void setup() {
-  pinMode(UP, INPUT_PULLUP);
-  digitalWrite(UP, HIGH);
-  pinMode(RIGHT, INPUT_PULLUP);
-  digitalWrite(RIGHT, HIGH);
-  pinMode(DOWN, INPUT_PULLUP);
-  digitalWrite(DOWN, HIGH);
-  pinMode(LEFT, INPUT_PULLUP);
-  digitalWrite(LEFT, HIGH);
+  pinMode(UP_PIN, INPUT_PULLUP);
+  digitalWrite(UP_PIN, HIGH);
+  pinMode(RIGHT_PIN, INPUT_PULLUP);
+  digitalWrite(RIGHT_PIN, HIGH);
+  pinMode(DOWN_PIN, INPUT_PULLUP);
+  digitalWrite(DOWN_PIN, HIGH);
+  pinMode(LEFT_PIN, INPUT_PULLUP);
+  digitalWrite(LEFT_PIN, HIGH);
 
-  
-
+  LCD.InitLCD();  
 
   Serial.begin(115200);
 }
@@ -63,10 +72,42 @@ void input(int buttonNumber) {
       delay(250);
       break;
     case 5:
+      Field.newGame();
+      Player.newGame();
+      delay(250);
       break;
   }
 }
 
 void output() {
-  
+  if (!(Field.getLoseTrigger())) {
+    LCD.disableSleep();
+    LCD.clrScr();
+
+    LCD.drawRect(0, 0, 12, 19);
+    LCD.setPixel(Player.getX(), Player.getY());
+    for (int i = 0; i < height - 2; i++) {
+      for (int j = 0; j < width - 2; j++) {
+        if (Field.getDataRows(i, j)) {
+          LCD.setPixel(i + 1, j + 1);
+        }
+      }
+    }
+    for (int i = 0; i < height - 2; i++) {
+      if (Field.getDataShellsX(i) != -1) {
+        LCD.setPixel(Field.getDataShellsX(i), Field.getDataShellsY(i));
+      }
+    }
+    LCD.update();
+    
+    LCD.print("POINTS", 14, 0);
+    LCD.printNumI(Field.getPoints(), 14, 8);
+    LCD.enableSleep();
+  }
+  else {
+    LCD.disableSleep();
+    LCD.clrScr();
+    LCD.print("GAME OVER", CENTER, 0);
+    LCD.enableSleep();
+  }
 }
